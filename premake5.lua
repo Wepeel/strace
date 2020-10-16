e13dff2ad3bb10c1d750406c8a78c058cc18566a
Genghis Khan <gkomet@g.jct.ac.il>
2020-10-16 11:52:13 +0300

    Added premake

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+workspace "strace"
+	architecture "x86_64"
+	configurations
+	{
+		"Release",
+		"Debug"
+	}
+
+	system "linux"
+	
+
+project "strace"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+
+	outputDir = "%{cfg.buildcfg}/%{cfg.system}/%{cfg.architecture}"
+
+	targetdir ("%{wks.location}/target/" .. outputDir .. "/%{prj.name}")
+    objdir ("%{wks.location}/intermediate/" .. outputDir .. "/%{prj.name}")
+
+	files
+    {
+        "inc/**.h",
+        "src/**.cpp"
+    }
+
+	includedirs
+	{
+		"inc"
+	}
+
+	filter "configurations:Debug"
+        symbols "on"
+        runtime "Debug"
+
+    filter "configurations:Release"
+        optimize "on"
+        runtime "Release"
